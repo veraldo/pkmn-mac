974b6ffa1abdb80efbc2e7a3572e99b5171c180d
veraldo <everaldogaliano@outlook.com>
2016-04-15 09:27:57 -0300

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,7 +1,7 @@
 abstract class Pokemon{
   private String name;
-  private long hp_max;
-  private long hp_actual;
+  private int hp_max;
+  private int hp_actual;
   private String type;
 
   abstract void scratch(Pokemon enemy);
