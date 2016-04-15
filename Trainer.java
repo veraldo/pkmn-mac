80ccff810e86f90fff48e34585f62cb4db8b5093
veraldo <everaldogaliano@outlook.com>
2016-04-15 11:16:09 -0300

    Update Trainer.java

--- Trainer.java
+++ Trainer.java
@@ -1,5 +1,18 @@
 private class Trainer{
   Pokemon party = new Pokemon [6];
+  
+    public class Attack extends Event{
+    private int damage;
+    private int type;
+    public int priority = 4;
+    ataca(Treiner1.party[n].type, Treiner2.party[n].type){
+      pokemon
+      if(pokemon1.type = pokemon2.type) pokemon2.hp_actual -= pokemon1.damage;
+      
+    }
+  
+    
+  }
 
 
 
