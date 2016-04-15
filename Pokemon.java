cb95d6d299dcb00de32ac9d1805fb0d7a72e1e58
veraldo <everaldogaliano@outlook.com>
2016-04-15 11:27:16 -0300

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -3,10 +3,5 @@ abstract class Pokemon{
   private final int hp_max;
   private int hp_actual;
   private String type;
-
-  abstract void scratch(Pokemon enemy);
-  abstract void impact(Pokemon enemy);
-  abstract void type_attack(Pokemon enemy);
-  
   
 }
