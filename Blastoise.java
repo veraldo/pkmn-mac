1fc6a380df49153853bb059fab1e612346cbdb98
veraldo <everaldogaliano@outlook.com>
2016-04-15 11:29:56 -0300

    Update Blastoise.java

--- Blastoise.java
+++ Blastoise.java
@@ -1,4 +1,8 @@
-public class Blastoise implements Pokemon{
+public class Blastoise extends Pokemon{
+  name = "Blastoise";
   hp_max = 150;
+  hp_actual = 150;
+  type = "water";
+  fainted = false;
   
 }
